bcb62b27a999f3650d4f2fc4fb911318614e2d25
juni8453 <juni8453@naver.com>
2021-07-13 22:56:15 +0900

    update 7_13 Home

--- src/vue-spring-jeon/src/views/TestPage.vue
+++ src/vue-spring-jeon/src/views/TestPage.vue
@@ -1,108 +1,117 @@
 <template>
-    <v-container fluid>
-        <v-row dense>
-            <v-col cols="12" md="12" sm="12">
-                <BwBar></BwBar>  
-                <v-card class="text-center">
-                  <v-row>
-                    <v-col cols="3" v-for="item in productlist" :key="item.pId">
-                      <v-card>
-                        <v-row>
-                          <v-col>
-                            <v-col>
-                              <v-card outlined>
-                                {{item.pId}}
-                              </v-card>
-                            </v-col>    
-                            <v-col>
-                              <v-card outlined>
-                                {{item.pName}}
-                              </v-card>
-                            </v-col>
-                            <v-col>
-                              <v-card>
-                                <v-img
-                                    height="250"
-                                    :src="`/images/thumb/${item.iPk}${item.iName}`"
-                                ></v-img> 
-                              </v-card>
-                            </v-col>    
-
-                          </v-col>    
-                        </v-row>
-                      </v-card>
-                    </v-col>
-                  </v-row>
-                </v-card>
-                <infinite-loading @infinite="infiniteHandler"></infinite-loading>
-                <!-- <v-card>
-                  {{new Date().getFullYear() + new Date().getMonth() + "/" + new Date().getDate()}}
-                </v-card> -->
-              
+  <v-container fluid>
+    <v-row dense>
+      <v-col cols="12" md="12" sm="12">
+        <BwBar></BwBar>  
+        <v-card class="text-center">
+          <v-row>
+            <v-col cols="3" v-for="item in $store.state.productlist" :key="item.pId">
+              <v-card>
+                <v-row>
+                  <v-col>
+                    <v-card outlined>
+                      {{item.pId}}
+                    </v-card>
+                    <v-card outlined>
+                      {{item.pName}}
+                    </v-card>
+                    <v-card>
+                      <v-img
+                          height="250"
+                          :src="`/images/thumb/${item.iPk}${item.iName}`"
+                      ></v-img> 
+                    </v-card>
+                  </v-col>    
+                </v-row>
+              </v-card>
             </v-col>
-            <v-footer>
-                <v-card>
-                    <v-card-text class="indigo lighten-1 white--text text-center" flat tile>
-                       <v-row>
-                           <v-col>
-                                Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                           </v-col>
-                       </v-row>
-                    </v-card-text>
-                    <v-divider></v-divider>
-                    <v-card-text class="black--text">
-                        {{ new Date().getFullYear() }} — <strong>Jeon Shopping Mall</strong>
-                    </v-card-text>
-                </v-card>
-            </v-footer>   
-        </v-row>
-    </v-container>
+          </v-row>
+        </v-card>
+        <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+      </v-col>
+      <v-footer>
+          <v-card>
+              <v-card-text class="indigo lighten-1 white--text text-center" flat tile>
+                  <v-row>
+                      <v-col>
+                          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                      </v-col>
+                  </v-row>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-text class="black--text">
+                  {{ new Date().getFullYear() }} — <strong>Jeon Shopping Mall</strong>
+              </v-card-text>
+          </v-card>
+      </v-footer>   
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import InfiniteLoading from 'vue-infinite-loading'
 import BwBar from '../components/BwBar.vue'
-import { mapState } from 'vuex'
+// import { mapState } from 'vuex'
 import axios from 'axios'
 
 export default {
 data() {
   return {
     limit:0,
-    testArray:[]
+    pageOpt:8,
+    productlist:[]
   }
 },
 
 created(){
   console.log('created 시작')
-  this.$store.dispatch('test', {limit:this.limit}) // mutation으로 productlist에 저장 해줘야함
+  this.$store.dispatch('test', {limit:this.limit}) 
 },
 
 computed:{
-    ...mapState(['productlist'])
+    // ...mapState(['productlist'])
+    name:{
+      set: function() {
+      },
+      get: function() {
+        return this.$store.state.productlist
+      }
+    } // Computed property was assigned to but it has no setter 오류 때문에 setter 추가
 },
 
 methods: {
   infiniteHandler($state){
-    axios.get(`http://localhost:9000/api/auth/test/${this.limit + 8}`)
+    axios.get(`http://localhost:9000/api/auth/test/${this.limit + this.pageOpt}`)
     .then(Response => {
+
       console.log('infiniteHandler Response.data를 받았습니다.')
-      console.log('Return 값'+Response.data) // limit 8,8 인 Response.data
-      console.log('Return 값 길이'+Response.data.list.length)
+      console.log('Response.data:', JSON.stringify(Response.data))
+      console.log('Response.data.list:', JSON.stringify(Response.data.list)) // 받아온 나머지 데이터
+      console.log('Response.data.list.length:', JSON.stringify(Response.data.list.length)) // 나머지 데이터 길이
+
       setTimeout(() => {
+        alert('무한스크롤 시작')
+
         if(Response.data.list.length) {
-          this.productlist = this.productlist.concat(Response.data.list) // 기존 productlist에 Response.data 합치기
-          console.log('this.productlist:'+this.productlist)
-          this.$store.commit('READ_PRODUCT_LIST', this.productlist)
+          console.log('현재 productlist' + JSON.stringify(this.$store.state.productlist)) // 0,8까지의 데이터
+          console.log('현재 productlist 길이' + JSON.stringify(this.$store.state.productlist.length)) // 8
+          this.$store.state.productlist = this.$store.state.productlist.concat(Response.data.list) 
+          // concat을 찾을 수 없음
+          // this.productlist.push(Response.data.list)
+          this.$store.commit('READ_PRODUCT_LIST', this.$store.state.productlist)
           $state.loaded()
-          if(this.productlist.list.length / 8 == 0) {
+
+          if(this.$store.state.productlist.length / this.pageOpt == 0) {
+            //list를 찾을 수 없음
             $state.complete()
           }
+
         } else {
           $state.complete()
         }
-      }, 1000)
+      },1000)
     })
+    
     .catch(error => {
       console.log(error)
     })
